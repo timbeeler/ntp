0d7c21299aa42a552555b4b3d248406e4ef2ac80
tbeeler <timothybeeler@me.com>
2015-06-23 15:12:52 -0700

    Set variables for timezones

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+#File Paths
+default[:ntp][:timezone][:pacific]       = "usr/share/zoneinfo/PST8PDT"
+default[:host][:timezone][:localtime]    = "etc/localtime"
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,11 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
+#Variables
+ntp_timezone_pacific      = node[:ntp][:timezone][:pacific]
+host_localtime            = node[:host][:localtime]
+
+
 package 'ntp' do
   action :install
 end
@@ -11,6 +16,14 @@ end
 cookbook_file 'etc/ntp.conf' do
   source 'ntp.conf'
   mode '644'
+end
+
+file host_localtime do
+  owner 'root'
+  group 'root'
+  mode 0644
+  content ::File.open(ntp_timezone_pacific).read
+  action :create
 end
 
 service 'ntpd' do
