e1d9cee5f9b2c1ba106b3d761d0b09bb0f5a601f
tbeeler <timothybeeler@me.com>
2015-07-06 14:17:53 -0700

    uses template instead of file

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,24 +5,27 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 #Variables
-timezone_pacific      = node[:timezone][:pacific]
+timezone_desired      = node[:timezone][:nyc]
 timezone_localtime    = node[:timezone][:localtime]
 
-
+# Install ntp
 package 'ntp' do
   action :install
 end
 
-cookbook_file 'etc/ntp.conf' do
-  source 'ntp.conf'
+# Deploy ntp.conf from template
+
+template 'etc/ntp.conf' do
+  source 'ntp.conf.erb'
   mode '644'
 end
 
+# Set timezone
 file timezone_localtime do
   owner 'root'
   group 'root'
   mode 0644
-  content ::File.open(timezone_pacific).read
+  content ::File.open(timezone_desired).read
   action :create
 end
 
