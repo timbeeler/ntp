58a56486a5648e35f0d20201b647780d8d3523c4
tbeeler <timothybeeler@me.com>
2015-07-06 14:17:14 -0700

    added time servers and more zones

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,12 @@
-#File Paths
-default[:timezone][:pacific]       = "usr/share/zoneinfo/PST8PDT"
+#Time Zones Files
+default[:timezone][:pst8pdt]       = "/usr/share/zoneinfo/PST8PDT"
+default[:timezone][:utc]       = "/usr/share/zoneinfo/UTC"
+default[:timezone][:cuperinto]  = "/usr/share/zoneinfo/US/Pacific"
+default[:timezone][:nyc]  = "/usr/share/zoneinfo/US/Eastern"
+
 default[:timezone][:localtime]           = "etc/localtime"
+
+#Time Servers
+default[:timeserver_apple] = "time.apple.com"
+default[:timeserver_pool_00] = "0.north-america.pool.ntp.org"
+default[:timeserver_pool_01] = "1.north-america.pool.ntp.org"
